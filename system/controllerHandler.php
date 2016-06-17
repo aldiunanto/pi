0f1fcca69ac48c3f85655172d01e4391f8259e90
Aldi Unanto <aldiunanto@yahoo.com>
2016-06-17 10:02:43 +0700

    _action exploded

--- system/controllerHandler.php
+++ system/controllerHandler.php
@@ -3,10 +3,10 @@
 session_start();
 require_once('connection.php');
 
-$param = explode('.', $_GET['c']);
+$param = explode('.', $_POST['_action'] ?: $_GET['_action']);
 require_once('../app/controllers/' . $param[0] . '.php');
 
 $classPath	= 'app\controllers\\' . $param[0];
-$controller = new $classPath();
+$controller = new $classPath($conn);
 
 print $controller->{$param[1]}();
